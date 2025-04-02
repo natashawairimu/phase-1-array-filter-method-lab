597c4f7f3ddef25e4a31f2e30769b0e57bd47312
natashanyokabi <natashawairimu27@gmail.com>
2025-04-03 00:51:16 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+const drivers = [
+    { name: "Bobby", hometown: "New York" },
+    { name: "Sammy", hometown: "Los Angeles" },
+    { name: "Sally", hometown: "Chicago" },
+    { name: "Annette", hometown: "Houston" },
+    { name: "Bobby", hometown: "Denver" }
+  ];
+  
+  function findMatching(drivers, query) {
+      return drivers.filter(function(driver) {
+          return driver.toLowerCase() === query.toLowerCase();
+      });
+  }
+  
+  function fuzzyMatch(drivers, query) {
+      return drivers.filter(function(driver) {
+          return driver.toLowerCase().startsWith(query.toLowerCase());
+      });
+  }
+  
+  function matchName(drivers, query) {
+      return drivers.filter(function(driver) {
+          return driver.name === query;
+      });
+  }
+ 
+
